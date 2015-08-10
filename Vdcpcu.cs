68e0eae4637db8ed8acb5d858ac5cb34933336ce
kalaharileeu <cfdelange@gmail.com>
2015-08-10 21:59:11 +1200

    new file added

---
+++ Vdcpcu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotDVT
+{
+    class Vdcpcu : Valuelist
+    {
+        private Dictionary<float, List<float>> slices;
+
+        public Vdcpcu(List<string> stringvaluelist) : base(stringvaluelist)
+        {
+            valuesstring = stringvaluelist;
+            slices = new Dictionary<float, List<float>>();
+        }
+
+        public Dictionary<float, List<float>> Slices 
+        {
+            get{ return slices; }
+        }
+
+        public void Populareslices(Dictionary<float, List<int>> slicedvalues)
+        {
+            foreach (KeyValuePair<float, List<int>> kv in slicedvalues)
+            {
+                slices.Add(kv.Key, valuesfloat.GetRange(kv.Value[0], kv.Value[1] - kv.Value[0]));
+            }
+        }
+
+        public new float GetAverage
+        {
+            get { return average; }
+        }
+
+        public new float GetMax
+        {
+            get { return maxvalue; }
+        }
+
+        public new float GetMin
+        {
+            get { return minvalue; }
+        }
+
+        public new List<float> GetFloats
+        {
+            get { return valuesfloat; }
+        }
+    }
+}
