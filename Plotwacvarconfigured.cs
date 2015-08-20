6dd14cb33ffbe265849bc52e1f770b9f505de4a0
kalaharileeu <cfdelange@gmail.com>
2015-08-20 22:03:50 +1200

    added plot

---
+++ Plotwacvarconfigured.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotDVT
+{
+    class Plotwacvarconfigured
+    {
+        private List<Baselist> columnobjects;
+        private Dictionary<float, List<float>> slices;
+
+        public Plotwacvarconfigured(List<Baselist> list)
+        {
+            columnobjects = new List<Baselist>();
+            columnobjects = list;
+            CreatSlices();
+        }
+
+        private void CreatSlices()
+        {
+            foreach (Baselist bl in columnobjects)
+            {
+                if (bl is Wacvarconfigured)
+                {
+                    slices = bl.GetSlices();
+                    try
+                    {
+                        //Find Vdcconfigure to get slice parameters
+                        foreach (Baselist bltwo in columnobjects)
+                        {
+                            if (bltwo is Vdcconfigured)
+                                bl.Populareslices((bltwo as Vdcconfigured).Slicelist);
+                        }
+                    }
+                    catch (InvalidCastException)
+                    {
+
+                    }
+                }
+            }
+        }
+
+        public void CreatSlices(float deg)
+        {
+            foreach (Baselist bl in columnobjects)
+            {
+                if (bl is Wacvarconfigured)
+                {
+                    try
+                    {
+                        //Find Vdcconfigure to get slice parameters
+                        foreach (Baselist bltwo in columnobjects)
+                        {
+                            if (bltwo is Vdcconfigured)
+                                bl.Populareslices((bltwo as Vdcconfigured).Slicelist, deg);
+                        }
+                    }
+                    catch (InvalidCastException)
+                    {
+
+                    }
+                }
+            }
+        }
+
+        public Dictionary<float, List<float>> GetSlices
+        {
+            get { return slices;}
+        }
+    }
+}
---
+++ Wacvarconfigured.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotDVT
+{
+    class Wacvarconfigured : Valuelist
+    {
+
+        public Wacvarconfigured(List<string> stringvaluelist)
+            : base(stringvaluelist)
+        {
+        }
+
+        public new float GetAverage
+        {
+            get { return average; }
+        }
+
+        public new float GetMax
+        {
+            get { return maxvalue; }
+        }
+
+        public new float GetMin
+        {
+            get { return minvalue; }
+        }
+
+        public new List<float> GetFloats
+        {
+            get { return valuesfloat; }
+        }
+
+        public Dictionary<float, List<float>> Slices
+        {
+            get { return slices; }
+        }
+    }
+}
