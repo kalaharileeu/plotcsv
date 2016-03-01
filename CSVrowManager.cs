7374c5853981a5a9ba35fe3f1456329179d0d5c5
cornel de lange <cfdelange@gmail.com>
2016-03-01 14:27:28 +1300

    added row manager that populates the rows

---
+++ CSVrowManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace PlotDVT
+{
+    class CSVrowManager
+    {
+        public CSVrowManager()
+        {
+        }
+
+        public void Load(List<Column> selectedcolumns)
+        {
+            this.selectedcolumns = selectedcolumns;
+            if(checkcolumnlengths())
+            {
+                populaterowdata();
+            }
+        }
+        //all the data counts must be the same
+        private bool checkcolumnlengths()
+        {
+            int length = selectedcolumns[0].Columnvalues.Count;
+            foreach (Column c in selectedcolumns)
+            {
+                if (length != c.Columnvalues.Count)
+                    return false;
+            }
+            return true;
+        }
+
+        private void populaterowdata()
+        {
+            rows.Add(new CSVrow());
+            for(int i = 0; i < selectedcolumns[0].Columnvalues.Count; i++)
+            {
+                //get reference to last inserted row
+                foreach (Column c in selectedcolumns)
+                    rows[rows.Count - 1].Addvlaue(c.alias, c.Columnvalues[i]);
+            }
+        }
+
+        private List<Column> selectedcolumns;
+        private List<CSVrow> rows;
+    }
+}
