0cec8422a135221ce9ba66cb6c17b9c3a1c4e4ec
cornel de lange <cfdelange@gmail.com>
2016-03-14 09:49:31 +1300

    updated layout and layout colors

--- Form1.Plot.cs
+++ Form1.Plot.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,35 +10,30 @@ namespace PlotDVT
     public partial class Form1 : Form
     {
         /// <summary>
-        /// Test paramteres should either come from database or some defaults needs to
-        /// be at ahand in the textbox if it cannot connect to the database
-        /// </summary>
-        private void settestparameters()
-        {
-            //values should come from database later
-            textBox5.Text = "60";//FS Vdc
-            textBox6.Text = "12";//FS Idc
-            textBox7.Text = "284";//FS Vac
-            textBox8.Text = "1.2";//FS Iac
-            textBox9.Text = "220";//FS Sac
-        }
-        /// <summary>
         /// Plots the Var vs real power triangle
         /// </summary>
         private async void async_col_sequenceplot()
         {
-            //List<YourType> newList = new List<YourType>(oldList);
-            chartdefaults();
+            //Clear all the data points
+            if (chart3.Series != null)
+            {
+                foreach (Series s in chart3.Series)
+                {
+                    clearseries(s);
+                }
+            }
+            await Task.Delay(100);
+            //chartdefaults();
             //clear value everytime button calls this function
             chart3.Series.Clear();
             chart3.Titles.Clear();
             //AC plots Set up axis variables
             chart3.ChartAreas[0].AxisX.Minimum = 0;
             chart3.ChartAreas[0].AxisX.Maximum = 300;
-            chart3.ChartAreas[0].AxisX.Interval = 20; // Whatever you like
-            chart3.ChartAreas[0].AxisY.Minimum = -300;
-            chart3.ChartAreas[0].AxisY.Maximum = 300;
-            chart3.ChartAreas[0].AxisY.Interval = 20; // Whatever you like
+            chart3.ChartAreas[0].AxisX.Interval = 40; // Whatever you like
+            chart3.ChartAreas[0].AxisY.Minimum = -280;
+            chart3.ChartAreas[0].AxisY.Maximum = 280;
+            chart3.ChartAreas[0].AxisY.Interval = 40; // Whatever you like
             //DC volt plots chart area 1
             chart3.ChartAreas[1].AxisY.Minimum = 0;
             chart3.ChartAreas[1].AxisY.Maximum = 50;
@@ -62,7 +56,6 @@ namespace PlotDVT
             List<float> Dcvcnf = new List<float>(colobjinterflist.First(item => item.GetName() == "Vdcconfigured").GetFloats());
             List<float> Dcvpm = new List<float>(colobjinterflist.First(item => item.GetName() == "Vdcpowermeter").GetFloats());
             List<float> Dcvpcu = new List<float>(colobjinterflist.First(item => item.GetName() == "Vdcpcu").GetFloats());
-
             //Get a fail list to do selective plotting
             List<bool> accuracyfaillist = new List<bool>
                 (Calculate.Faillist(WACpcu, Wacpm, 1, float.Parse(textBox9.Text), 1));
@@ -80,57 +73,66 @@ namespace PlotDVT
             chart3.Series["Vdcconfigured"].ChartArea = "ChartArea2";
             ////configure series plots
             Series Waccnfseries = chart3.Series["Wacconfigured"];
-            chart3.Series["Wacpowermeter"].ChartType = SeriesChartType.Line;
-            chart3.Series["Wacpowermeter"].BorderWidth = 1;
-            chart3.Series["Wacpowermeter"].IsVisibleInLegend = false;
+            Series DCvpm = chart3.Series["DCvpm"];
+            Series Vdcconfigured = chart3.Series["Vdcconfigured"];
+            Series Wacpowermeter = chart3.Series["Wacpowermeter"];
+            Wacpowermeter.ChartType = SeriesChartType.Line;
+            Wacpowermeter.BorderWidth = 1;
+            Wacpowermeter.IsVisibleInLegend = false;
             Waccnfseries.ChartType = SeriesChartType.Point;
             Waccnfseries.MarkerStyle = MarkerStyle.Cross;
             Waccnfseries.MarkerSize = 6;
             Waccnfseries.IsVisibleInLegend = false;
-            chart3.Series["DCvpm"].ChartType = SeriesChartType.Point;
-            chart3.Series["DCvpm"].MarkerStyle = MarkerStyle.Circle;
-            chart3.Series["DCvpm"].BorderWidth = 6;
-            chart3.Series["DCvpm"].IsVisibleInLegend = false;
-            chart3.Series["Vdcconfigured"].ChartType = SeriesChartType.Point;
-            chart3.Series["Vdcconfigured"].MarkerStyle = MarkerStyle.Cross;
-            chart3.Series["Vdcconfigured"].MarkerSize = 6;
-            chart3.Series["Vdcconfigured"].IsVisibleInLegend = false;
+            DCvpm.ChartType = SeriesChartType.Point;
+            DCvpm.MarkerStyle = MarkerStyle.Circle;
+            DCvpm.BorderWidth = 6;
+            DCvpm.IsVisibleInLegend = false;
+            Vdcconfigured.ChartType = SeriesChartType.Point;
+            Vdcconfigured.MarkerStyle = MarkerStyle.Cross;
+            Vdcconfigured.MarkerSize = 6;
+            Vdcconfigured.IsVisibleInLegend = false;
             Wacpowerbuglist = new Bugs();
             Wdcvbuglist = new Bugs();
             for (int i = 0; i < Wacpm.Count; i++)
             {
                 if(accuracyfaillist[i] == false || accuracyfaillistvar[i] == false)
                 {
-                    Wacpowerbuglist.Addbug(CSVrowManager.GetaCSVrow(i));
-                    //Plot the congfigured w/va powermeter values
-                    chart3.Series["Wacpowermeter"].Points.AddXY(0, 0);
-                    chart3.Series["Wacpowermeter"].Points.AddXY(Wacpm[i], 0);
-                    chart3.Series["Wacpowermeter"].Points.AddXY(Wacpm[i], Wvarpm[i]);
-                    chart3.Series["Wacpowermeter"].Points.AddXY(0, 0);
-                    ////Plot the congfigured w/va configured values
-                    chart3.Series["Wacconfigured"].Points.AddXY(Waccnf[i], 0);
-                    chart3.Series["Wacconfigured"].Points.AddXY(Waccnf[i], Wvarcnf[i]);
-                    //Plot the congfigured Vdcpm/cnf powermeter values
-                    //chart3.Series["DCvpm"].Points.AddXY(0, 0);
-                    //chart3.Series["DCvpm"].Points.AddXY(Dcvpm[i], Waccnf[i]);
-                    //chart3.Series["Vdcconfigured"].Points.AddXY(Dcvcnf[i], Waccnf[i]);
-                    //await Task.Delay(Convert.ToInt16(textBox2.Text));
-                    ////Clear all the data points
-                    //if (chart3.Series != null)
-                    //{
-                    //    foreach (Series s in chart3.Series)
-                    //    {
-                    //        clearseries(s);
-                    //    }
-                    //}
-                }
+                    //ignore the values that is below this value in textbox10
+                    if (Wacpm[i] > float.Parse(textBox10.Text))
+                    {
+                        Wacpowerbuglist.Addbug(CSVrowManager.GetaCSVrow(i));
+                        //Plot the congfigured w/va powermeter values
+                        chart3.Series["Wacpowermeter"].Points.AddXY(0, 0);
+                        chart3.Series["Wacpowermeter"].Points.AddXY(Wacpm[i], 0);
+                        chart3.Series["Wacpowermeter"].Points.AddXY(Wacpm[i], Wvarpm[i]);
+                        chart3.Series["Wacpowermeter"].Points.AddXY(0, 0);
+                        ////Plot the congfigured w/va configured values
+                        chart3.Series["Wacconfigured"].Points.AddXY(Waccnf[i], 0);
+                        chart3.Series["Wacconfigured"].Points.AddXY(Waccnf[i], Wvarcnf[i]);
+                    }
+                 }
 
                 if(accuracyfaillistdcv[i] == false)
                 {
                     Wdcvbuglist.Addbug(CSVrowManager.GetaCSVrow(i));
-                    chart3.Series["DCvpm"].Points.AddXY(Dcvpm[i], Waccnf[i]);
-                    chart3.Series["Vdcconfigured"].Points.AddXY(Dcvcnf[i], Waccnf[i]);
+                    chart3.Series["DCvpm"].Points.AddY(Dcvpm[i]);
+                    chart3.Series["Vdcconfigured"].Points.AddY(Dcvcnf[i]);
                 }
+            }
+
+            List<string> valuesforreport = new List<string>()
+            {
+            "Wacvarconfigured",
+            "Wacconfigured",
+            "Wdcconfigured",
+            "Vdcconfigured",
+            "Phaseconfigured",
+            "Temperature"
+            };
+            if (Wacpowerbuglist != null)
+            {
+                foreach (CSVrow row in Wacpowerbuglist.Bugrows)
+                    richTextBox1.AppendText(row.Humantext(valuesforreport) + "\r\n");
             }
         }
 
